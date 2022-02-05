d9b2b0a2439726a1f1ddd0abdacbcd805c3a7343
eviltik <eviltik@gmail.com>
2022-02-05 12:06:37 +0100

    add stuff

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,12 +1,18 @@
 AFRAME.registerComponent('boot', {
+
+    multiple: false,
+
     init: function () {
 
+        const IS_VR_AVAILABLE = AFRAME.utils.device.isMobile() || window.hasNonPolyfillWebVRSupport;
+
         this.el.sceneEl.addEventListener('renderstart', this.onRenderStart);
-        
+        this.onRenderStart = AFRAME.utils.bind(this.onRenderStart, this);  
     },
 
     onRenderStart: function () {
         console.log('boot:onRenderStart');
         AFRAME.log('boot:onRenderStart');
     }
+
 });
